aa65cc4efc0653171fcce928b11120e62756f69a
maste <maste@DESKTOP-N7TT9HL>
2022-11-07 21:57:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите номер дня недели от 1 до 7 включительно: ");
+int week = Convert.ToInt32(Console.ReadLine());
+switch (week)
+    {
+    case 1:
+        Console.WriteLine(day.Понедельник);
+        break;
+    case 2:
+        Console.WriteLine(day.Вторник);
+        break;
+    case 3:
+        Console.WriteLine(day.Среда);
+        break;
+    case 4:
+        Console.WriteLine(day.Четверг);
+        break;
+    case 5:
+        Console.WriteLine(day.Пятница);
+        break;
+    case 6:
+        Console.WriteLine(day.Суббота);
+        break;
+    case 7:
+        Console.WriteLine(day.Вокресенье);
+        break;
+    default:
+        Console.WriteLine("Такого дня нет, в днях недели. Введите чисто от 1 до 7 включительно");
+        break;
+    }
+enum day
+{
+    Понедельник,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Вокресенье
+}
